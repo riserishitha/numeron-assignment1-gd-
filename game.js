f56c178d5142ad08f79e9cf5fb25311cfd6da5ca
riserishitha <rishitha.n@kalvium.community>
2023-12-04 17:06:02 +0530

    commit changes

--- game.js
+++ game.js
@@ -1,5 +1,78 @@
-// Iteration 2: Generate 2 random number and display it on the screen
+var referredtime = 0;
+let num1 = document.getElementById('number1')
+let num2 = document.getElementById('number2')
+let timered = document.getElementById('timer')
+reset();
+let score = 0;
+let no1 = Math.floor(Math.random()*100)
+let no2 = Math.floor(Math.random()*100)
+num1.innerHTML = no1;
+num2.innerHTML = no2;
 
-// Iteration 3: Make the options button functional
+let great = document.getElementById('greater-than');
+great.addEventListener('click',function(){
+    if (no1>no2){
+        score = score+1;
+        no1 = Math.floor(Math.random()*100)
+        no2 = Math.floor(Math.random()*100)
+        num1.innerHTML = no1;
+        num2.innerHTML = no2;
+        clearInterval(referredtime);
+        reset();
+        console.log("Correct")
+    }else{
+        localStorage.setItem('score',score);
+        window.location.href="./gameover.html";
+        console.log("wrong")
+    }
+})
 
-// Iteration 4: Build a timer for the game
+let equal = document.getElementById('equal-to');
+equal.addEventListener('click',function(){
+    if (no1==no2){
+        score = score+1;
+        no1 = Math.floor(Math.random()*100)
+        no2 = Math.floor(Math.random()*100)
+        num1.innerHTML = no1;
+        num2.innerHTML = no2;
+        clearInterval(referredtime);
+        reset();
+        console.log("Correct")
+    }else{
+        localStorage.setItem('score',score);
+        window.location.href="./gameover.html";
+        console.log("wrong")
+    }
+})
+
+let less = document.getElementById('lesser-than');
+less.addEventListener('click',function(){
+    if (no1<no2){
+        score = score +1;
+        no1 = Math.floor(Math.random()*100)
+        no2 = Math.floor(Math.random()*100)
+        num1.innerHTML = no1;
+        num2.innerHTML = no2;
+        clearInterval(referredtime);
+        reset();
+        console.log("Correct")
+    }else{
+        localStorage.setItem('score',score);
+        window.location.href="./gameover.html";
+        console.log("wrong")
+    }
+})
+
+function reset(){
+    var t = 5;
+    timered.innerHTML= t;
+    referredtime = setInterval(()=>{
+        t = t -1;
+        timered.innerText = t;
+        if (t==0){
+            localStorage.setItem("score",score);
+            window.location.href= './gameover.html'
+        }
+    },1000)
+
+}
